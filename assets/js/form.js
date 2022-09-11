cf2cf1cecb1467c769a0e3cf3435f310b1531497
pascalcorrea <98128852+pascalcorrea@users.noreply.github.com>
2022-09-11 02:08:01 -0300

    form-js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,34 +1,28 @@
-var cajaNombre = document.querySelector("#name");
-    var cajaApellidos = document.querySelector("#surname");
-    var cajaEmail = document.querySelector("#email");
-    var cajaNumero = document.querySelector("#number");
-    var cajaContraseña = document.querySelector("#password");
-
-    var buttonEnviar = document.querySelector("#button");
-
-    function imprimirDatos() {
-    alert("mi primer onclick event")
-    alert(cajaNombre)
-    }
-
-    buttonEnviar.addEventListener("click", imprimirDatos());
-
-    $('#contactForm').on('submit', function (event) {
+$('#contactForm').on('submit', function (event) {
     event.preventDefault(); // prevent reload
 
+    var cajaNombre = $('#caja-texto-nombre').val();
+    var cajaApellido = $('#caja-texto-apellido').val();
+    var cajaEmail = $('#caja-texto-email').val();
+    var cajaTelefono = $('#caja-texto-telefono').val();
+    var cajaContraseña = $('#caja-texto-contraseña').val();
+
+    alert(cajaNombre, cajaApellido, cajaEmail, cajaTelefono, cajaContraseña);
+
     var formData = new FormData(this);
-    formData.append('service_id', 'service_xa42f1q');
-    formData.append('template_id', 'template_hvkervc');
-    formData.append('user_id', 'W8wCKti6oi2hoY0_W');
+    formData.append('service_id', 'service_3ovfx7i');
+    formData.append('template_id', 'vapzstore-template');
+    formData.append('user_id', '0cwP3lmSkSPgGPpK5');
 
     $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
         type: 'POST',
         data: formData,
-        contentType: false, // auto-detection
-        processData: false // no need to parse formData to string
+      contentType: false, // auto-detection
+      processData: false // no need to parse formData to string
     }).done(function () {
         alert('Your mail is sent!');
     }).fail(function (error) {
         alert('Oops... ' + JSON.stringify(error));
     });
-    });
+
+});
